91dbdcbb180a34b1d1030f3dc21f76ecc43b75f1
Arsharaj <arsharajchauhan343@gmail.com>
2020-05-22 15:49:01 +0530

    Add binary search tree

---
+++ trees/binary_search_trees.c
@@ -0,0 +1,32 @@
+// Include all the required files
+#include"binary_search_trees.h"
+
+// Main function - Starting point of the program
+int main(){
+    int choice,elem;
+
+    printf("\tBinary Search Tree\n\n");
+    do{
+        printf("1. Insert a value in the binary search tree\n");
+        printf("2. Search for a value in our binary search tree\n");
+        printf("0. Exit\n");
+        printf("Enter your choice : ");
+        scanf("%d",&choice);
+        switch(choice){
+            case 0 : break;
+            case 1:
+                printf("Enter the element you want to insert : ");
+                scanf("%d",&elem);
+                rootptr = InsertNode(rootptr,elem);
+                break;
+            case 2:
+                printf("Enter element to search : ");
+                scanf("%d",&elem);
+                SearchNode(rootptr,elem) ? printf("Found..\n") : printf("Not Found..\n");
+                break;
+            default: printf("Enter a valid choice!!\n");
+        }
+    }while(choice!=0);
+    printf("\n");
+    return 0;
+}
---
+++ trees/binary_search_trees.h
@@ -0,0 +1,36 @@
+/*
+Trees are a very important data structure. They are used to store data in a non-linear way. They are best suited for
+storing the data in a hierarchial order. These trees have a wide range of applications. They are used in implementing 
+file systems as well an network modulation algorithm. Trees are the core of computer science alogithms. Trees make it 
+possible to store the data of millions and billions of users in an efficient way.
+
+These trees are so efficient because they provide operations on element in O(log n) time.
+There are many types of trees based on their logical representation as well as their different properties.
+Some of the most common are:
+1. Binary Search Trees - Trees in which the values of the left subtree is less than or equal to the root node and the 
+values in the right subtree is greater than root node.
+*/
+
+#ifndef _BINARY_SEARCH_TREES
+#define _BINARY_SEARCH_TREES
+
+// Built-in libraries
+#include<stdio.h>
+#include<stdlib.h>
+
+// Standard Node in a binary search tree
+struct BstNode{
+    int data;
+    struct BstNode* left;
+    struct BstNode* right;
+};
+
+// Global reference to this tree data structure
+struct BstNode* rootptr=NULL;
+
+// Custom Libraries
+#include"get_node.h"
+#include"insert_elem.h"
+#include"search_elem.h"
+
+#endif
